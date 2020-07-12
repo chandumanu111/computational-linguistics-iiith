805049c0d555919524961bc6be8fdf7501632d52
manuchandu111 <manasakrishna121@gmail.com>
2020-07-12 21:11:34 +0530

    sentences were added

--- RDIP/Codes/function.js
+++ RDIP/Codes/function.js
@@ -1,107 +1,109 @@
-function Function1() {
-    var text =
-      '{"employees":[' +
-      '{"a":"John ate an apple before afternoon <br>","b":"before afternoon John ate an apple <br>","c":"John before afternoon ate an apple <br>" },' +
-      '{"a":"some students like to study in the night<br>","b":"at night some students like to study" },' +
-      '{"a":"John and Mary went to church <br>","b":"Mary and John went to church" },' +
-      '{"a":"John went to church after eating <br>","b":"after eating John went to church <br>" ,"c":"John after eating went to church"},' +
-      '{"a":"John ate an apple so did she <br>","b":"she ate an apple so did John" },' +
-      '{"a":"John goes to the library and studies <br>","b":"John studies and goes to the library<br>" },' +
-      '{"a":"the teacher returned the book after she noticed the error <br>","b":"the teacher noticed the error after she returned the book<br>","c":"after the teacher returned the book she noticed the error<br>","d":"after the teacher noticed the error she returned the book<br>","e":"she returned the book after the teacher noticed the error<br>","f":"she noticed the error after the teacher returned the book<br>","g":" after she returned the book the teacher noticed the error<br>","h":" after she noticed the error the teacher returned the book<br>"},' +
-      '{"a":"the woman who called my sister sells cosmetics<br>","b":"the woman who sells cosmetics called my sister<br>" ,"c":"my sister who sells cosmetics called the woman<br>","d":"my sister who called the woman sells cosmetics"}]}';
+var eng1 = ['John ate an apple before afternoon',
+                    'before afternoon John ate an apple',
+                    'John before afternoon ate an apple'
+                ];
+var eng2 = ['some students like to study in the night',
+                    'at night some students like to study'
+                ];
+var eng3 = ['John and Mary went to church',
+                    'Mary and John went to church'
+                ];
+ var eng4 = ['John went to church after eating',
+                    'after eating John went to church',
+                    'John after eating went to church'
+                ];
+var eng5 = ['did he go to market',
+                    'he did go to market'
+                ];
+var eng6 = ['the woman who called my sister sells cosmetics',
+                    'the woman who sells cosmetics called my sister',
+                    'my sister who sells cosmetics called the woman',
+                    'my sister who called the woman sells cosmetics'
+                ];
+var eng7 = ['John goes to the library and studies',
+                    'John studies and goes to the library'
+                ];
+ var eng8 = ['John ate an apple so did she',
+                    'she ate an apple so did John'
+                ];
+ var eng9 = ['the teacher returned the book after she noticed the error',
+                    'the teacher noticed the error after she returned the book',
+                    'after the teacher returned the book she noticed the error',
+                    'after the teacher noticed the error she returned the book',
+                    'she returned the book after the teacher noticed the error',
+                    'she noticed the error after the teacher returned the book',
+                    'after she returned the book the teacher noticed the error',
+                    'after she noticed the error the teacher returned the book'
+                ];
+ var eng10 = ['I told her that I bought a book yesterday',
+                    'I told her yesterday that I bought a book',
+                    'yesterday I told her that I bought a book',
+                    'I bought a book that I told her yesterday',
+                    'I bought a book yesterday that I told her',
+                    'yesterday I bought a book that I told her'
+                ];
 
-    var text1 =
-      '{"e":[' +
-      '{"a":"राम और श्याम बाजार गयें<br>","c":"राम और श्याम गयें बाजार <br>","c":"बाजार गयें राम और श्याम<br>","d":"गयें बाजार राम और श्याम<br>" },' +
-      '{"a":"the woman who called my sister sells cosmetics<br>","b":"the woman who sells cosmetics called my sister<br>" ,"c":"my sister who sells cosmetics called the woman<br>","d":"my sister who called the woman sells cosmetics"}]}';
 
-    obj = JSON.parse(text);
-    document.getElementById("output").innerHTML =
-      obj.employees[0].a + " " + obj.employees[0].b + obj.employees[0].c;
 
-    document.getElementById("output1").innerHTML =
-      obj.employees[1].a + " " + obj.employees[1].b;
-    document.getElementById("output2").innerHTML =
-      obj.employees[2].a + " " + obj.employees[2].b;
-    document.getElementById("output3").innerHTML =
-      obj.employees[3].a + " " + obj.employees[3].b + " " + obj.employees[3].c;
-    document.getElementById("output4").innerHTML =
-      obj.employees[4].a + " " + obj.employees[4].b;
-    document.getElementById("output5").innerHTML =
-      obj.employees[5].a + " " + obj.employees[5].b;
-    document.getElementById("output6").innerHTML =
-      obj.employees[6].a +
-      " " +
-      obj.employees[6].b +
-      " " +
-      obj.employees[6].c +
-      " " +
-      obj.employees[6].d +
-      " " +
-      obj.employees[6].e +
-      " " +
-      obj.employees[6].f +
-      " " +
-      obj.employees[6].g +
-      " " +
-      obj.employees[6].h;
-    document.getElementById("output7").innerHTML =
-      obj.employees[7].a +
-      " " +
-      obj.employees[7].b +
-      " " +
-      obj.employees[7].c +
-      " " +
-      obj.employees[7].d;
+  var english = [eng1,eng2,eng3,eng4,eng5,eng6,eng7,eng8,eng9,eng10];
 
-    var hindi = [
-      "राम और श्याम बाजार गयें <br>",
-      "  राम और श्याम गयें बाजार<br>",
-      "बाजार गयें राम और श्याम<br>",
-      " गयें बाजार राम और श्याम<br><br>",
 
-      " राम सोया और श्याम भी<br>",
-      "श्याम सोया और राम भी<br>",
-      "  सोया श्याम और राम भी<br>",
-      " सोया राम और श्याम भी<br><br>",
 
-      " मैंने उसे बताया कि राम सो रहा है<br>",
-      "मैंने उसे बताया कि सो रहा है राम<br>",
-      "उसे मैंने बताया कि राम सो रहा है<br>",
-      "उसे मैंने बताया कि सो रहा है राम<br>",
-      "मैंने बताया उसे कि राम सो रहा है<br>",
-      "मैंने बताया उसे कि सो रहा है राम<br>",
-      "उसे बताया मैंने कि राम सो रहा है<br>",
-      "उसे बताया मैंने कि सो रहा है राम<br>",
-      "बताया मैंने उसे कि राम सो रहा है<br>",
-      "बताया मैंने उसे कि सो रहा है राम<br>",
-      "बताया उसे मैंने कि राम सो रहा है<br>",
-      "बताया उसे मैंने कि सो रहा है राम<br><br>",
+ var hin1 = ['राम और श्याम बाजार गयें',
+                    'राम और श्याम गयें बाजार',
+                    'बाजार गयें राम और श्याम',
+                    'गयें बाजार राम और श्याम'
 
-      "राम खाकर सोया<br>",
-      "खाकर राम सोया<br>",
-      "राम सोया खाकर<br>",
-      "खाकर सोया राम<br>",
-      "सोया राम खाकर<br>",
-      "सोया खाकर राम<br><br>",
+                ];
+ var hin2 = ['राम सोया और श्याम भी',
+                    'श्याम सोया और राम भी',
+                    'सोया श्याम और राम भी',
+                    'सोया राम और श्याम भी'
+                ];
+ var hin3 = ['मैंने उसे बताया कि राम सो रहा है',
+                    'मैंने उसे बताया कि सो रहा है राम',
+                    'उसे मैंने बताया कि राम सो रहा है',
+                    'उसे मैंने बताया कि सो रहा है राम',
+                    'मैंने बताया उसे कि राम सो रहा है',
+                    'मैंने बताया उसे कि सो रहा है राम',
+                    'उसे बताया मैंने कि राम सो रहा है',
+                    'उसे बताया मैंने कि सो रहा है राम',
+                    'बताया मैंने उसे कि राम सो रहा है',
+                    'बताया मैंने उसे कि सो रहा है राम',
+                    'बताया उसे मैंने कि राम सो रहा है',
+                    'बताया उसे मैंने कि सो रहा है राम'
+                ];
+  var hin4 = ['राम खाकर सोया',
+                    'खाकर राम सोया',
+                    'राम सोया खाकर',
+                    'खाकर सोया राम',
+                    'सोया राम खाकर',
+                    'सोया खाकर राम'
+                ];
+var hin5= ['बिल्लियों को मारकर कुत्ता सो गया	',
+                    'मारकर बिल्लियों को कुत्ता सो गया',
+                    'बिल्लियों को मारकर सो गया कुत्ता',
+                    'मारकर बिल्लियों को सो गया कुत्ता',
+                    'कुत्ता सो गया बिल्लियों को मारकर',
+                    'कुत्ता सो गया मारकर बिल्लियों को',
+                    'सो गया कुत्ता बिल्लियों को मारकर',
+                    'सो गया कुत्ता मारकर बिल्लियों को'
+                ];
+var hin6 = ['एक लाल किताब वहाँ है',
+                    'एक लाल किताब है वहाँ',
+                    'वहाँ है एक लाल किताब',
+                    'है वहाँ एक लाल किताब'
+                ];
+var hin7 = ['एक बड़ी सी किताब वहाँ है',
+                    'एक बड़ी सी किताब है वहाँ',
+                    'बड़ी सी एक किताब वहाँ है',
+                    'बड़ी सी एक किताब है वहाँ',
+                    'वहाँ है एक बड़ी सी किताब',
+                    'वहाँ है बड़ी सी एक किताब',
+                    'है वहाँ एक बड़ी सी किताब',
+                    'है वहाँ बड़ी सी एक किताब'
+                ];
 
-      "बिल्लियों को मारकर कुत्ता सो गया<br>",
-      "मारकर बिल्लियों को कुत्ता सो गया	<br>",
-      "बिल्लियों को मारकर सो गया कुत्ता	<br>",
-      "मारकर बिल्लियों को सो गया कुत्ता<br>",
-      "कुत्ता सो गया बिल्लियों को मारकर	<br>",
-      "कुत्ता सो गया मारकर बिल्लियों को	<br>",
-      "सो गया कुत्ता बिल्लियों को मारकर<br>",
-      "सो गया कुत्ता मारकर बिल्लियों को<br><br>",
 
-      "एक लाल किताब वहाँ है<br>",
-      "एक लाल किताब है वहाँ<br>",
-      "वहाँ है एक लाल किताब<br>",
-      "है वहाँ एक लाल किताब<br><br>",
 
-      "एक बड़ी सी किताब वहाँ है	<br>",
-      "एक बड़ी सी किताब है वहाँ<br>",
-      "बड़ी सी एक किताब वहाँ है<br>",
-      "बड़ी सी एक किताब है वहाँ<br>",
-      "वहाँ है एक बड़ी सी किताब<br>",
-      "वहाँ है बड़ी सी एक किताब<br
+ var hindi = [hin1,hin2,hin3,hin4,hin5,hin6,hin7];
